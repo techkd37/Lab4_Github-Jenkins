8e49d2912dfff5f0410e6bafdd56db9d552afe21
Kalid Mohamed <Kalid@umich.edu>
2025-02-06 14:46:47 -0500

    Changed Hello.java

--- Hello.java
+++ Hello.java
@@ -1,9 +1,13 @@
 public class Hello {
 	public static void main(String[] args) {
 
+        System.out.println("Start of Counting");
+
 		for(int i=1;i<=10;i++) {
 
 			System.out.println("Hello World..."+i);
+            
 		}
+        System.out.println("End of Counting");
 	}
 }
